5c113a34073ae581f10b7dc5292e2dd76ee030fe
Martin <mato.stefko@gmail.com>
2020-06-01 10:02:00 +0200

    Added typescript react template

---
+++ client-app/src/App.tsx
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  state = {
+    values: []
+  }
+
+  componentDidMount() {
+    this.setState({
+      values: [{ id: 1, name: 'Value 101' }, { id: 2, name: 'Value 102' }]
+    })
+  }
+
+
+  render() {
+    return (
+      <div className='App'>
+        <header className='App-header'></header>
+        <img src={logo} className='App-logo' alt="logo" />
+        <ul>
+          {this.state.values.map((value: any) => (
+            <li>{value.name}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+
+};
+
+
+
+
+export default App
+
