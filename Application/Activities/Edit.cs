a028252836b213c68589c4bc2ff7b488fdf91a24
Martin <mato.stefko@gmail.com>
2020-06-15 05:29:43 +0200

    Application of new <ErrorHandlingMiddleware>

--- Application/Activities/Edit.cs
+++ Application/Activities/Edit.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
+using Application.Errors;
 using FluentValidation;
 using MediatR;
 using Persistence;
@@ -53,7 +55,7 @@ namespace Application.Activities
                 var activity = await _context.Activities.FindAsync(request.Id);
 
                 if (activity == null)
-                    throw new Exception("Could not find activity");
+                    throw new RestException(HttpStatusCode.NotFound, new { activity = "Not found" });
                 //check if there is activity in request objext if not just just maintain current state of variable 
 
                 activity.Title = request.Title ?? activity.Title;
